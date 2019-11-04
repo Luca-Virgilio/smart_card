3e621bf798262478f4c3353a4289680815dbd444
Luca Virgilio <luca.virgilio95@gmail.com>
2019-11-04 10:23:44 +0100

    pug pages

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,7 +1,33 @@
 const homepageController = (req, res) => {
-    res.render('index', { title: 'Express ciao' });
-    };
+    res.render('index', {
+        title: 'e-voting System',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus perspiciatis consequatur, odit hic voluptas molestias, cum officia facere doloribus voluptatem velit ratione explicabo fugit nesciunt repellendus dolor necessitatibus culpa blanditiis.'
+    }
+    )
+};
+
+const loginController = (req, res) => {
+    res.render('login',
+        {
+            title: 'e-voting System',
+            description: 'Tutti i dati ramaranno'
+        })
+};
+
+const resultController = (req, res) => {
+    res.render('result',
+        {title: 'e-voting System'})
+};
+
+const questionController = (req, res) => {
+    res.render('question',
+        {title: 'e-voting System'})
+};
+
 
 module.exports = {
-    homepageController
+    homepageController,
+    loginController,
+    resultController,
+    questionController
 }
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,4 +5,8 @@ const ctrlMain = require('../controllers/main');
 /* GET home page. */
 router.get('/', ctrlMain.homepageController);
 
+router.get('/login', ctrlMain.loginController);
+router.get('/result', ctrlMain.resultController);
+router.get('/login/question', ctrlMain.questionController);
+
 module.exports = router;
