520fe048a9d494662a367930bbb39d15213ad800
Luca Virgilio <luca.virgilio95@gmail.com>
2019-11-05 14:26:24 +0100

    operation with mongo

--- app_server/controllers/vote.js
+++ app_server/controllers/vote.js
@@ -1,12 +1,8 @@
 // import for access database
 const mongoose = require('mongoose');
 const UserDB = mongoose.model('User');
-
-// only for emitter
-const EventEmitter = require('events');
-class MyEmitter extends EventEmitter {}
-
-const myEmitter = new MyEmitter();
+// hash function
+const crypto = require('crypto');
 
 
 // version http without engine
@@ -16,28 +12,65 @@ const votingController = (req, res) => {
 
     res
         .status(201)
-        .json({ msg: 'vote '+req.body.vote});
+        .json({ msg: 'vote ' + req.body.vote });
 };
 
-// simulate event emitter 
-const eventController = (req, res) =>{
+// simulate event 
+const eventController = async (req, res) => {
     console.log(req.body.name + ' try to access page...');
-    //myEmitter.emit(req.body.name);
-    addUser(req.body.name);
-    
-    res
-        .json('user created');
+    //addUser(req.body.name);
+    //getUser(req.body.name, res);
+    const hash = crypto.createHash('sha256').update(req.body.name).digest('base64');
+    try {
+        const result = await UserDB.find({ name: hash });
+        console.log(result);
+        if (result.length > 0) {
+            res.redirect('/question.html');
+        } else {
+            res.redirect('/login.html');
+        }
+    } catch (error) {
+
+    }
+
 }
 
 // insert user into the database
-const addUser = (user) =>{
+const addUser = async (user) => {
     console.log('creation ...');
-    UserDB.create({
-        name: user
-    }, (err, User)=>{
-        if (err) throw err;
-    });
+    const hash = crypto.createHash('sha256').update(user).digest('base64');
+    try {
+        const result = await UserDB.find({ name: hash });
+    console.log(hash);
+    // if the user isn't registered
+    if (result.length == 0) {
+        UserDB.create({
+            name: hash
+        }, (err, User) => {
+            if (err) throw err;
+        });
+    }
+    } catch (error) {
+        
+    }
 };
+
+const getUser = async (user, res) => {
+    const query = { name: user };
+    try {
+        result = await UserDB.find(query);
+        console.log(result);
+        if (result.length > 0) {
+            res.redirect('/question.html');
+        } else {
+            res.redirect('/login.html');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+
 
 module.exports = {
     votingController,
