5e17c36e8c132fb663a3aeb6c6c1858def2e6c1e
Luca Virgilio <luca.virgilio95@gmail.com>
2019-11-05 08:38:50 +0100

    fix voting javascript function

--- app_server/controllers/vote.js
+++ app_server/controllers/vote.js
@@ -1,10 +1,10 @@
 // version http without engine
 const votingController = (req, res) => {
     console.log('body:');
-    console.log(req.body);
+    console.log(req.body.vote);
 
     res
-        .json({ msg: 'vote ' });
+        .json({ msg: 'vote '+req.body.vote});
 };
 
 module.exports = {
